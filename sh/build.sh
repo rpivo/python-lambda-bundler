b07c128ae53a934d38f86d77266d3474b4273970
Ryan <ryanpivovar@gmail.com>
2021-03-06 13:23:07 -0500

    add conditional logic to check if any python packages were installed prior to creating the bundle

--- sh/build.sh
+++ sh/build.sh
@@ -8,5 +8,9 @@ python3.8 -m pip install -U pip;
 # Ex: pip3 install pytrends --target ./bind-mount/package --upgrade;
 
 # zip
-cd bind-mount/package && zip -r ../lambda.zip . && cd ..;
-zip -g lambda.zip lambda_function.py;
+if [ -z "$(ls -A bind-mount/package)" ]; then
+  cd bind-mount && zip lambda.zip lambda_function.py
+else
+  cd bind-mount/package && zip -r ../lambda.zip . && cd ..
+  zip -g lambda.zip lambda_function.py
+fi
